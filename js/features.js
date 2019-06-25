bd008993c078b5692e29b683e404c9f7750c0ca4
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-06-25 14:04:00 -0500

    added tab selector

--- js/features.js
+++ js/features.js
@@ -3,8 +3,17 @@ constructor(link){
 this.link = link;
 this.data = link.dataset.tab;
 this.contentElement = document.querySelector(`.feature-content[data-tab="${this.data}]`);
-this.
+this.tabContent = new this.TabContent(this.contentElement);
+link.addEventListener('click',() => this.select())
+};
+
+select(){
+    const tabs = document.querySelectorAll('.feature-tab');
+    Array.from(tabs).forEach(item => item.classList.remove('feature-tab-selected'));
+    this.tab.classList.add('feature-tabselected');
+    this.TabContent.select();
 }
+
 }
 
 
