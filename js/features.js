acee028f86587c0cd8b933afb630bf6731695ab0
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-06-25 16:11:03 -0500

    the tabs component is built and ready for styling

--- js/features.js
+++ js/features.js
@@ -1,18 +1,47 @@
-class TabLink{
+
+class FeatureTab {
     constructor(tab) {
-        this.tab = tab;
-        this.data = tab.dataset.tab;
-        this.contentElement = document.querySelector(`.feature-content[data-tab='${this.data}']`);
-        this.tabContent = new this.tabContent(this.contentElement);
-        tab.addEventListener('click', () => this.select())
+      this.tab = tab;
+      this.data = tab.dataset.tab 
+      console.log('this.data',this.data);
+      this.itemElement = document.querySelector(`.features-content[data-tab="${this.data}"]`);
+      console.log('this.itemElement',this.itemElement);
+      this.featureContent = new FeatureContent(this.itemElement);
+      console.log(this.featureContent)
+      tab.addEventListener('click', () => this.select())
+  
     };
-    select(){
-        const tabs = document.querySelectorAll('.')
+  
+    select() {
+  
+      const tabs = document.querySelectorAll('.features-tab');
+      Array.from(tabs).forEach(item => item.classList.remove('features-tab-selected'));
+      this.tab.classList.add('features-tab-selected');
+      this.featureContent.select();
     }
-}
+  }
+  
+  class FeatureContent {
+    constructor(element) {
+       this.element = element;
+    }
+  
+    select() {
+      const contents = document.querySelectorAll('.features-content');
+      Array.from(contents).forEach(item => item.classList.remove('features-content-selected'));
+      this.element.classList.add('features-content-selected');
+      
+    }
+  }
 
-let topTabs = document.querySelectorAll('.feature-tab')
-
-topTabs.forEach(tab) => {
-    new TabLink(tab)
-};
+  
+  let tabs = document.querySelectorAll('.features-tab')
+  
+  tabs.forEach((tab) => {
+    console.log(tabs)
+     new FeatureTab(tab) 
+    
+    });
+   
+  
+  
